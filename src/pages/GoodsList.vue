a38064d4164331e4fe6f06358cd4ebddcdfbca4d
qzr <z5021996@vip.qq.com>
2017-10-18 18:00:44 +0800

    10.18

---
+++ src/pages/GoodsList.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+      这是商品列表页面
+      <span>{{$route.params.goodsId}}</span>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import GoodsList from './../pages/GoodsList.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
-      path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      path: '/goods/:goodsId',
+      name: 'GoodsList',
+      component: GoodsList
     }
   ]
 })
